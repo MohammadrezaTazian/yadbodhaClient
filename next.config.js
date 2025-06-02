9e7583b51fbcdf31542b49efa6546d4eddfbc42f
Mohammad Reza Taziyan <Tazian@motorsazan.com>
2025-06-02 10:13:18 +0330

    add

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // فعال‌سازی خروجی استاتیک
   eslint: {
     ignoreDuringBuilds: true, // نادیده گرفتن خطاهای eslint هنگام ساخت پروژه
   },
